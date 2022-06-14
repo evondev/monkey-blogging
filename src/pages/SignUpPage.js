e2fcb54692976bccfaee4a22f458c8a2633f71c1
evondev <trananhtuan400@gmail.com>
2022-06-14 15:41:39 +0700

    Add role admin to handle action with user

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -54,8 +54,8 @@ const SignUpPage = () => {
       username: slugify(values.fullname, { lower: true }),
       avatar:
         "https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-      status: userStatus.ACTIVE,
-      role: userRole.USER,
+      status: Number(userStatus.ACTIVE),
+      role: Number(userRole.USER),
       createdAt: serverTimestamp(),
     });
 
